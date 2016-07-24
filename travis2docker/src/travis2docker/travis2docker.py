cbd72229ed26a1a61022db8af0bdb1a78d3d8a3b
Moisés López <moylop260@vauxoo.com>
2016-07-24 09:45:55 -0500

    with templating jinja2

--- travis2docker/src/travis2docker/travis2docker.py
+++ travis2docker/src/travis2docker/travis2docker.py
@@ -5,6 +5,7 @@ import stat
 from tempfile import gettempdir
 
 import yaml
+import jinja2
 
 RE_ENV_STR = r"(?P<var>[\w]*)[ ]*[\=][ ]*[\"\']{0,1}" + \
              r"(?P<value>[\w\.\-\_/\$\{\}\:,\(\)\#\* ]*)[\"\']{0,1}"
@@ -30,9 +31,21 @@ class Travis2Docker(object):
         with open(yml_path, "r") as f_yml:
             return yaml.load(f_yml)
 
-    def __init__(self, yml_path, image, work_path=None, dockerfile=None):
+    @property
+    def dockerfile_template(self):
+        template_dir, template_name = os.path.split(self.template_path)
+        env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
+        return env.get_template(template_name)
+
+    def __init__(self, yml_path, image, work_path=None, dockerfile=None,
+                 template_path=None):
         if dockerfile is None:
             dockerfile = 'Dockerfile'
+        if template_path is None:
+            template_path = os.path.join(
+                os.path.dirname(os.path.realpath(__file__)),
+                'templates', 'Dockerfile')
+        self.template_path = template_path
         self.image = image
         self._sections = collections.OrderedDict()
         self._sections['env'] = 'env'
@@ -75,8 +88,8 @@ class Travis2Docker(object):
 
     def _compute_run(self, data, section):
         args = self._make_script(data, section)
-        args['cmds'].append('RUN %(dst)s' % args )
-        return '\n'.join(args['cmds'])
+        args['runs'] = ['%(dest)s' % args]
+        return args
 
     @staticmethod
     def chmod_execution(file_path):
@@ -89,19 +102,15 @@ class Travis2Docker(object):
             for var, value in self.curr_exports:
                 f_section.write('\nexport %s=%s' % (var, value))
             for line in data:
-                # if self.re_export.findall(line):
-                #     print "section", section, "line", line
-                #     import pdb;pdb.set_trace()
                 self.curr_exports.extend([
                     (var, value)
                     for _, _, var, value in self.re_export.findall(line)])
                 f_section.write('\n' + line)
         args = {
-            'src': os.path.relpath(file_path, self.curr_work_path),
-            'dst': "/" + section,
+            'src': "./" + os.path.relpath(file_path, self.curr_work_path),
+            'dest': "/" + section,
         }
         self.chmod_execution(file_path)
-        args['cmds'] = ["COPY %(src)s %(dst)s" % args]
         return args
 
     def reset(self):
@@ -110,10 +119,12 @@ class Travis2Docker(object):
 
     def _compute_entrypoint(self, data, section):
         args = self._make_script(data, section)
-        return '\n'.join(args['cmds'])
+        return args
 
     def compute_dockerfile(self):
+        # TODO: If env is not defined
         for count, env in enumerate(self._compute('env'), 1):
+            kwargs = {'runs': [], 'copies': []}
             self.curr_work_path = os.path.join(self.work_path, str(count))
             if not os.path.isdir(self.curr_work_path):
                 os.mkdir(self.curr_work_path)
@@ -123,19 +134,29 @@ class Travis2Docker(object):
             entryp_relpath = os.path.relpath(entryp_path, self.curr_work_path)
             with open(curr_dockerfile, "w") as f_dockerfile, \
                     open(entryp_path, "w") as f_entrypoint:
-                f_dockerfile.write("FROM " + self.image + "\n")
-                f_dockerfile.write(env + "\n")
-                f_dockerfile.write("COPY " + entryp_relpath + " /entrypoint.sh\n")
+                kwargs['image'] = self.image
+                kwargs['entrypoint'] = entryp_relpath
+                kwargs['env'] = env
                 for section, type_section in self._sections.items():
                     if section == 'env':
                         continue
                     result = self._compute(section)
                     if not result:
                         continue
-                    f_dockerfile.write(result + "\n")
-                    if type_section == 'entrypoint':
-                        f_entrypoint.write("/" + section + '\n')
-                f_dockerfile.write("ENTRYPOINT /entrypoint.sh\n")
+                    if isinstance(result, dict):
+                        src, dest = result.get('src'), result.get('dest')
+                        if src and dest:
+                            kwargs['copies'].append((src, dest))
+                        runs = result.get('runs')
+                        if runs:
+                            kwargs['runs'].extend(runs)
+                        elif src and dest:
+                            f_entrypoint.write(dest + '\n')
+                dockerfile_content = \
+                    self.dockerfile_template.render(kwargs).strip('\n ')
+                # print "dockerfile_content", dockerfile_content
+                f_dockerfile.write(dockerfile_content)
+                # f_entrypoint.write()
             self.chmod_execution(entryp_path)
         self.reset()
 
@@ -147,3 +168,7 @@ if __name__ == '__main__':
     t2d = Travis2Docker(yml_path, 'vauxoo/odoo-80-image-shippable-auto')
     t2d.compute_dockerfile()
     print t2d.work_path
+    # print t2d.dockerfile_template
+    # kwargs = {'image': 'vauxoo'}
+    # dockerfile_content = t2d.dockerfile_template.render(kwargs)
+    # print dockerfile_content
