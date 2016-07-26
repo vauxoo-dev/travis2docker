de1da3e528b31b5e300b9dd51b24ab2124a1a72d
Moisés López <moylop260@vauxoo.com>
2016-07-26 15:46:08 -0500

    Delete old folder

--- old/setup.py
+++
@@ -1,8 +0,0 @@
-
-import setuptools
-
-
-setuptools.setup(setup_requires=['pbr'],
-                 pbr=True,
-                 test_suite="travis2docker.test",
-                 package_data={'': ['*.yaml']})
